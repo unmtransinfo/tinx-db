30e5ee8858740b9c9fb55a5e4ec1196c3d9496cc
Jack-42 <97483169+Jack-42@users.noreply.github.com>
2026-03-24 12:14:25 -0600

    add docker resource constraints

--- pull_db.sh
+++ pull_db.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
+# Docker resource constraints
+CPUS=8
+MEMORY=32G
+
 # MySQL user params
 MYSQL_ROOT_PASSWORD="root_pass" # modify if using in prod
 MYSQL_DATABASE="tinx" # database name
 DB_USER="tinx_user" # read-only user
 DB_PASSWORD="user_pass" # modify if using in prod
 
-# MySQL params - you will need to tune these based on your available resources (defaults are for 32GB RAM, 8 cores)
+# MySQL params - you will need to tune these based on your available resources (defaults are for 32GB RAM, 8 cpus)
 INNODB_BUFFER_POOL_SIZE=20G # InnoDB buffer pool — most important knob, use ~60-70% of RAM
-INNODB_BUFFER_POOL_INSTANCES=8 # 1 per core 
-INNODB_READ_IO_THREADS=8 # 1 per core
-INNODB_WRITE_IO_THREADS=8 # 1 per core
-INNODB_PURGE_THREADS=4 # 1/2 per core
+INNODB_BUFFER_POOL_INSTANCES=8 # 1 per cpu 
+INNODB_READ_IO_THREADS=8 # 1 per cpu
+INNODB_WRITE_IO_THREADS=8 # 1 per cpu
+INNODB_PURGE_THREADS=4 # 1/2 per cpu
 INNODB_REDO_LOG_CAPACITY=4G
 INNODB_LOG_BUFFER_SIZE=256M
 INNODB_FLUSH_METHOD="O_DIRECT"
@@ -25,6 +29,8 @@ JOIN_BUFFER_SIZE=32M
 # TODO: replace this with dockerhub repo once pushed
 docker build -t tinx-db .
 docker run -d \
+  --cpus ${CPUS} \
+  --memory ${MEMORY} \
   -p 3306:3306 \
   -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
   -e MYSQL_DATABASE=${MYSQL_DATABASE} \
