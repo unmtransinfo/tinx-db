94c7755f584aa2788c9996efaca4555ff4f73bbb
Jack-42 <97483169+Jack-42@users.noreply.github.com>
2026-04-21 14:12:14 -0600

    fix name of parent->parent_id

--- src/sql/08_add_disease_ancestors.sql
+++ src/sql/08_add_disease_ancestors.sql
@@ -7,7 +7,7 @@
 /* Indexes for performance. Note that do_parent.doid is not unique and cannot
  * be a primary key. */
 CREATE INDEX do_parent_doid_idx ON do_parent (doid);
-CREATE INDEX do_parent_parent_idx ON do_parent (parent);
+CREATE INDEX do_parent_parent_idx ON do_parent (parent_id);
 CREATE INDEX tinx_disease_doid_idx ON tinx_disease (doid);
 
 /* Create the table. We assume a max depth of 14. (The longest observed was 10) */
@@ -22,19 +22,19 @@ CREATE TABLE tinx_disease_ancestors (
   COALESCE(p14.doid, p13.doid, p12.doid, p10.doid, p9.doid, p8.doid, p7.doid, p6.doid, p5.doid, p4.doid, p3.doid, p2.doid, p1.doid) AS max_ancestor,
   CONCAT_WS(' / ', p14.doid, p13.doid, p12.doid, p10.doid, p9.doid, p8.doid, p7.doid, p6.doid, p5.doid, p4.doid, p3.doid, p2.doid, p1.doid) AS ancestor_path
 FROM do_parent p1
-LEFT JOIN do_parent p2 ON p2.doid = p1.parent
-LEFT JOIN do_parent p3 ON p3.doid = p2.parent
-LEFT JOIN do_parent p4 ON p4.doid = p3.parent
-LEFT JOIN do_parent p5 ON p5.doid = p4.parent
-LEFT JOIN do_parent p6 ON p6.doid = p5.parent
-LEFT JOIN do_parent p7 ON p7.doid = p6.parent
-LEFT JOIN do_parent p8 ON p8.doid = p7.parent
-LEFT JOIN do_parent p9 ON p9.doid = p8.parent
-LEFT JOIN do_parent p10 ON p10.doid = p9.parent
-LEFT JOIN do_parent p11 ON p11.doid = p10.parent
-LEFT JOIN do_parent p12 ON p12.doid = p11.parent
-LEFT JOIN do_parent p13 ON p13.doid = p12.parent
-LEFT JOIN do_parent p14 ON p14.doid = p13.parent;
+LEFT JOIN do_parent p2 ON p2.doid = p1.parent_id
+LEFT JOIN do_parent p3 ON p3.doid = p2.parent_id
+LEFT JOIN do_parent p4 ON p4.doid = p3.parent_id
+LEFT JOIN do_parent p5 ON p5.doid = p4.parent_id
+LEFT JOIN do_parent p6 ON p6.doid = p5.parent_id
+LEFT JOIN do_parent p7 ON p7.doid = p6.parent_id
+LEFT JOIN do_parent p8 ON p8.doid = p7.parent_id
+LEFT JOIN do_parent p9 ON p9.doid = p8.parent_id
+LEFT JOIN do_parent p10 ON p10.doid = p9.parent_id
+LEFT JOIN do_parent p11 ON p11.doid = p10.parent_id
+LEFT JOIN do_parent p12 ON p12.doid = p11.parent_id
+LEFT JOIN do_parent p13 ON p13.doid = p12.parent_id
+LEFT JOIN do_parent p14 ON p14.doid = p13.parent_id;
 
 
 ALTER TABLE tinx_disease_metadata
